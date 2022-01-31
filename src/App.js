b9d29220ebe3918525651d217d52b6fa3bfa2e28
Bernardo Gomes <bgomesdeabreu@gmail.com>
2022-01-31 15:38:14 -0300

    Remove extra files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import "./App.css";
-import SuggestPyProjectPackageVersionUpdate from "./suggestPyProjectPackageVersionUpdate.jsx";
+import React, { useEffect, useState } from "react";
+import { pyProjectData, versionsData } from "./exampleData";
+import { producePinnedVersions } from "./businessRules";
+import ColArea from "./components/colArea";
 
-function App() {
+const App = () => {
+  const [pyProjectFile, SetPyProjectFile] = useState(pyProjectData);
+  const [newPyProjectFile, SetNewPyProjectFile] = useState(pyProjectData);
+  const [isPyProjectFileWithProblem, SetIsPyProjectFileWithProblem] =
+    useState(false);
+  const [versions, SetVersions] = useState(versionsData);
+
+  function pyProjectFileHandleChange(event) {
+    SetPyProjectFile(event.target.value);
+  }
+  function versionsHandleChange(event) {
+    SetVersions(event.target.value);
+  }
+
+  useEffect(() => {
+    const { content, hasError } = producePinnedVersions(
+      pyProjectFile,
+      versions
+    );
+    SetIsPyProjectFileWithProblem(hasError);
+    SetNewPyProjectFile(content);
+  }, [pyProjectFile, versions]);
+
   return (
-    <div className="container-fluid">
-      <SuggestPyProjectPackageVersionUpdate />
+    <div className="row">
+      <ColArea
+        title="PyProject.toml"
+        value={pyProjectFile}
+        onChange={pyProjectFileHandleChange}
+        hasAlert={isPyProjectFileWithProblem}
+        alertMessage="PyProject.toml with problem."
+      />
+      <ColArea
+        title="poetry show"
+        value={versions}
+        onChange={versionsHandleChange}
+      />
+      <ColArea
+        title="New PyProject.toml"
+        value={newPyProjectFile}
+        readOnly={true}
+      />
     </div>
   );
-}
+};
 
 export default App;
